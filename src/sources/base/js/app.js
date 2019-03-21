6c44d0018e351f299bb817c20847f4d8b991db5a
oksana <dompers123@gmail.com>
2019-03-21 17:39:29 +0200

    fix-recaptcha-onsubmit

--- src/sources/base/js/app.js
+++ src/sources/base/js/app.js
@@ -1,71 +1,6 @@
-/**
- * Vanilla JS
- */
-
-/*
-console.log('%cWelcome to Quick Dev Template!', 'color: green');
-
-
-var displayVersion = function(name, version, installed) {
-    console.log(name + ': %c' + version, 'color: ' + (installed ? 'green' : 'red'));
-};
-
-displayVersion('jQuery', (typeof jQuery != 'undefined' ? ('v' + jQuery.fn.jquery) : 'not installed!'), typeof jQuery != 'undefined');
-displayVersion('Angular', (typeof angular != 'undefined' ? ('v' + angular.version.full) : 'not installed!'), typeof angular != 'undefined');
-displayVersion('Angular2', (typeof ng != 'undefined' ? 'v2.1.0' : 'not installed!'), typeof ng != 'undefined');
- */
-
-function fix_frame(block, lastScrollTop, topOfWindow){
-
-    var block_fix = block;
-    var success_story_offset = $(block_fix).closest('.success_story').offset().top;
-    var success_story_end = success_story_offset + $(block_fix).closest('.success_story').height();
-
-
-    var topOfWindow = topOfWindow;
-
-
-    if (topOfWindow > lastScrollTop){
-        if (topOfWindow > (success_story_offset) && (topOfWindow) < (success_story_end - $(block_fix).find('.frame').height())){
-            $(block_fix).removeClass('bottom').addClass('fix');
-        }
-        else if((topOfWindow) >= (success_story_end - $(block_fix).find('.frame').height())){
-            $(block_fix).removeClass('fix').addClass('bottom');
-        }
-    }
-    if (topOfWindow < lastScrollTop){
-        if((topOfWindow) < (success_story_end  - $(block_fix).find('.frame').height()) && (topOfWindow > (success_story_offset))){
-            $(block_fix).addClass('fix').removeClass('bottom');
-        }
-        else if (topOfWindow < (success_story_offset) ){
-            $(block_fix).removeClass('fix');
-        }
-    }
-};
-
-function change_in_fix(block, n, scrollCount) {
-    if(scrollCount > n * 20 && scrollCount < n * 40){
-        $(block).find('.nav_story li').each(function (i, el) {
-            $(el).removeClass('active');
-        });
-        $(block).find('.nav_story li').eq(n + 1).addClass('active');
-    }
-}
-
 function validateElement(element){
 
-    //var nameReg = /^([a-zА-Яа-я]+[,.]?[ ]?|[a-zА-Яа-я]+['-]?)+$/;
     var nameReg = /^[^0-9]+$/;
-
-    //var numberReg = /^(?:(?:\+?[0-9]{1,3}\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/;
-    /*
-    numbers like this:
-     +14(555)5555555
-     +380635555555
-     555-555-5555
-     +1-555-532-3455
-     */
-
     var numberReg = /^(?:\+?)[0-9]+$/;
     var emailReg = /^\S+@\S+$/;
 
@@ -166,7 +101,6 @@ $('.wrapper_tab_video video').hover(function toggleControls() {
         this.setAttribute("controls", "controls")
     }
 });
-
 $(document).ready(function(){
 
     $( 'a[data-toggle="tab"]' ).on('shown.bs.tab', function(e) {
@@ -201,42 +135,6 @@ $(document).ready(function(){
 
         validateElement($(this).closest('.form-group').find('.form-control'));
     });
-
-    $('#formSubmit').click(function(){
-        $.post($("#formContact").attr('action'), JSON.stringify({
-            name: $("#formContact input[name='name']").val(),
-            email: $("#formContact input[name='email']").val(),
-            phone: $("#formContact input[name='phone']").val(),
-            organization: $("#formContact input[name='organization']").val(),
-            message: $("#formContact textarea[name='message']").val(),
-            'g-recaptcha-response': $("#formContact textarea[name='g-recaptcha-response']").val()
-        }), function (data) {
-            $(".thanks").show();
-            $("#formContact button").hide();
-        }, 'json');
-    });
-});
-
-var lastScrollTop = 0;
-var scrollCount = 0;
-var success_story_1 = $('.success_story_1');
-var success_story_1_offset = $(success_story_1).offset().top;
-var success_story_2 = $('.success_story_2').offset().top;
-
-$(document).scroll(function() {
-    var id = '';
-    var part = 0;
-    var topOfWindow = $(window).scrollTop();
-
-    if(topOfWindow > lastScrollTop){
-
-    }
-    else{
-
-    }
-
-    lastScrollTop = topOfWindow;
-
 });
 
 $('.site_footer .nav_category .title').on('click', function () {
