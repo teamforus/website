9ed72142d3119c173f4171e9ef681e56bd10d890
Jamal <30194799+jamalv@users.noreply.github.com>
2019-04-28 11:36:40 +0200

    add analytics snippet

--- src/sources/base/js/app.js
+++ src/sources/base/js/app.js
@@ -185,3 +185,19 @@ $("#modalContact").on('hidden.bs.modal', function (e) {
 $(window).on('load', function () {
     $('#formSubmitHome').prop('disabled','disabled');
 });
+
+
+// Analytics snippet
+$(window).on('load', function () {
+    var _paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//dev.analytics.forus.io/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '2']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
+});
